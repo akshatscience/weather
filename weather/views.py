3956e4f21fc79fb4824d0dacf7476e965720ce23
akshatscience <akshat.science@gmail.com>
2019-10-24 00:31:02 +0530

    update

--- weather/views.py
+++ weather/views.py
@@ -15,7 +15,7 @@ def index(request):
 
     cities = weather.objects.all()
     weather_data=[]
-    for city in cities:
+    for city in cities[:1]:
 
         r = requests.get(url.format(city)).json()
         city_weather = {
@@ -32,27 +32,30 @@ def search(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=2ef7677a9f31d3a93404cd663fe966dd'
 
 
-    cities = weather.objects.all()
-    for i in cities:
+    #cities = weather.objects.all()
+    data=[]
+    if request.method=="GET":
+        s=request.GET["search"]
+
+        city = weather.objects.get(name = s)
         
-
-        city = weather.objects.get(name = i)
-
         r = requests.get(url.format(city)).json()
-
+        
 
         city_weather = {
                     'city':city,
                     'temprature':r['main']['temp'],
                     'description':r['weather'][0]['description'],
                     'icon':r['weather'][0]['icon']
-                }    
-        return render(request,'search.html',{'city_weather':city_weather})
+                }  
+        data.append(city_weather) 
+        
+        return render(request,'search.html',{'city_weathers':data})
 
 
     
 
-def get_queryset(self): # new
-        return weather.objects.filter(
-            Q(name__icontains=weather.objects.all()) | Q(state__icontains='NY')
-        )
+#def get_queryset(self): # new
+ #       return weather.objects.filter(
+  #          Q(name__icontains=weather.objects.all()) | Q(state__icontains='NY')
+   #     )
